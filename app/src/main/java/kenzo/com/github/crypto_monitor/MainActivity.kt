93723ddcdd8241f5fb4f0a2e52f93c672f1b3a09
André Kenzo Acchor Tanaka <andre.tanaka@fiap.com.br>
2024-10-04 10:23:19 -0300

    Configurando a mainactivity.kt para executar com layout pronto

--- app/src/main/java/kenzo/com/github/crypto_monitor/MainActivity.kt
+++ app/src/main/java/kenzo/com/github/crypto_monitor/MainActivity.kt
@@ -1,21 +1,21 @@
 package kenzo.com.github.crypto_monitor
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import kenzo.com.github.crypto_monitor.ui.theme.CryptomonitorTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
     }
 }
